becf4b1864b5db3b527f3789d9066cf9a70fe858
KR <79979949+justfoolingaround@users.noreply.github.com>
2024-10-29 18:47:45 +0545

    Chore: Remove unwarranted `print` logging.

--- aether_server/routes/views/index_view.py
+++ aether_server/routes/views/index_view.py
@@ -1,18 +1,11 @@
 import aiohttp.web as web
 import aiohttp_jinja2
 
-
 from aether_server.routes.routes_decl import generic_routes
-import pathlib
-
-HTML = pathlib.Path(__file__).parent.parent.parent / "index.html"
 
 
 @generic_routes.view("/")
 class AetherIndexView(web.View):
     @aiohttp_jinja2.template("index.html")
     async def get(self):
-        ws_url = self.request.app.router["websocket"].url_for()
-        print("ws url", ws_url)
-
-        return {"ws_url": ws_url}
+        return {"ws_url": self.request.app.router["websocket"].url_for()}
