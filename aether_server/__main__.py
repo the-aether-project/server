3ee9f98297783173a97e86863f80e438f38ae9cd
KR <79979949+justfoolingaround@users.noreply.github.com>
2024-10-29 18:45:36 +0545

    Chore: Standarization

--- aether_server/__main__.py
+++ aether_server/__main__.py
@@ -1,20 +1,24 @@
+import pathlib
+
 import aiohttp.web as web
 import aiohttp_jinja2
 import jinja2
 
 from aether_server.routes import generic_routes
 
-import pathlib
-
-BASE_DIR = pathlib.Path(__file__).parent
-
 
 def create_app(_) -> web.Application:
-    app = web.Application()
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("aether_server/templates"))
+    project_root = pathlib.Path(__file__).parent
 
-    app.router.add_static("/static/", path=str(BASE_DIR / "static"), name="static")
+    app = web.Application()
+
+    app.router.add_static("/static/", path=project_root / "static", name="static")
     app.add_routes(generic_routes)
+
+    aiohttp_jinja2.setup(
+        app, loader=jinja2.FileSystemLoader(project_root / "templates")
+    )
+
     return app
 
 
