409cabc7d75059007f7e520e30bd7ed37d8605d6
KR <79979949+justfoolingaround@users.noreply.github.com>
2024-11-05 20:43:57 +0545

    Feat: GitHub `OAuth` authentication endpoint

--- aether_server/__main__.py
+++ aether_server/__main__.py
@@ -1,17 +1,36 @@
 import pathlib
 
+import aiohttp
 import aiohttp.web as web
 import aiohttp_jinja2
 import jinja2
 
 from aether_server.routes import generic_routes
-from aether_server.routes.utils import RTC_APPKEY, RTCPeerManager
+from aether_server.routes.utils import HTTP_CLIENT_APPKEY, RTC_APPKEY, RTCPeerManager
+
+try:
+    import dotenv
+
+    dotenv.load_dotenv()
+except ImportError:
+    ...
+
+try:
+    import aiohttp_debugtoolbar
+except ImportError:
+    aiohttp_debugtoolbar = None
 
 
 async def peer_manager_clearing_ctx(app: web.Application):
     app[RTC_APPKEY] = RTCPeerManager()
     yield
     await app[RTC_APPKEY].close()
+
+
+async def http_client_clearing_ctx(app: web.Application):
+    app[HTTP_CLIENT_APPKEY] = aiohttp.ClientSession()
+    yield
+    await app[HTTP_CLIENT_APPKEY].close()
 
 
 def create_app(_) -> web.Application:
@@ -21,11 +40,14 @@ def create_app(_) -> web.Application:
 
     app.router.add_static("/static/", path=project_root / "static", name="static")
     app.add_routes(generic_routes)
-    app.cleanup_ctx.append(peer_manager_clearing_ctx)
+    app.cleanup_ctx.extend((peer_manager_clearing_ctx, http_client_clearing_ctx))
 
     aiohttp_jinja2.setup(
         app, loader=jinja2.FileSystemLoader(project_root / "templates")
     )
+
+    if aiohttp_debugtoolbar is not None:
+        aiohttp_debugtoolbar.setup(app, intercept_redirects=False)
 
     return app
 
--- aether_server/routes/utils/__init__.py
+++ aether_server/routes/utils/__init__.py
@@ -1,8 +1,9 @@
+import aiohttp
 import aiohttp.web as web
 
 from .rtc import RTCPeerManager
 
 RTC_APPKEY = web.AppKey("rtc_peer_manager", RTCPeerManager)
-
+HTTP_CLIENT_APPKEY = web.AppKey("http_client", aiohttp.ClientSession)
 
 __all__ = ["RTCPeerManager", "RTC_APPKEY"]
