99a833741bf3352d1f9e7596f3c9722aaf4eae52
ostrich-egg <sauhardhakafle12@gmail.com>
2025-01-07 14:27:06 +0545

    Removing unnecessary file

--- aether_server/routes/views/__init__.py
+++ aether_server/routes/views/__init__.py
@@ -2,12 +2,10 @@ from .authentication_view import AetherGitHubAuthenticationView
 from .crud_view import AetherComputersView, AetherIdentificationView
 from .index_view import AetherIndexView
 from .middleware import Authorize_middleware
-from .webrtc_view import AetherWebRTCView
 from .websocket_view import AetherLandlordCommunicate
 
 __all__ = [
     "AetherIndexView",
-    "AetherWebRTCView",
     "AetherGitHubAuthenticationView",
     "Authorize_middleware",
     "AetherComputersView",
--- aether_server/routes/views/webrtc_view.py
+++
@@ -1,3 +0,0 @@
-class AetherWebRTCView:
-    async def post(self, landlords, ws_client, client_id, offer, landlord_id):
-        pass
